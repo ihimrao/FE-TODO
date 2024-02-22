d2491b9f14ccff67427fb0a35a67babc122cabec
Himanshu Yadav <ihimrao@gmail.com>
2024-02-22 17:51:44 +0530

    added docker file

--- src/Pages/Chat/Auth.jsx
+++ src/Pages/Chat/Auth.jsx
@@ -1,5 +1,5 @@
 import { Box, InputAdornment, TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 
@@ -14,7 +14,7 @@ const PageWrapper = styled.div`
 const ImageContainer = styled(Box)`
   width: 90%;
   height: 40vh;
-  border-radius: 10px;
+  border-radius: 20px;
   margin-top: 5vh;
   background-image: url('https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg');
   background-size: cover;
@@ -27,8 +27,9 @@ const BlurredMask = styled(Box)`
   width: 100%;
   height: 100%;
   bottom: 0;
-  background-color: rgba(0, 0, 255, 0.5);
-  border-radius: 20px; /* To match the border radius of the ImageContainer */
+  font-weight: 900;
+  background-color: rgba(173, 64, 255, 0.5);
+  border-radius: 20px;
 `;
 const InputContainer = styled(Box)`
   position: absolute;
@@ -37,35 +38,56 @@ const InputContainer = styled(Box)`
   width: 70%;
   transform: translate(-50%, -50%);
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 10px; /* Adjust the space between the input and the button */
 `;
+const TodoList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin-top: 20px;
+`;
+
+const TodoItem = styled.li`
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+`;
 const ToDo = () => {
+  const [todos, setTodos] = useState(["sd", "sd", "sdd"]);
   return (
     <PageWrapper>
       <ImageContainer>
         <BlurredMask />
       </ImageContainer>
       <InputContainer>
+        <p style={{ width: "100%", fontWeight: "900", fontSize: "30px", color: "wheat" }}>TODOS</p>
         <TextField
           variant="outlined"
           placeholder="Type here..."
           fullWidth
+          style={{ background: "white", borderRadius: "20px" }}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
                 <button
+                  style={{ color: "white", background: "#87CEEB", border: "none", borderRadius: "5px", height: "30px", width: "80px" }}
                   // onClick={handleButtonClick}
                   edge="end"
-                >Submit
+                >Create
                 </button>
               </InputAdornment>
             ),
           }}
         />
       </InputContainer>
+      <TodoList>
+        {todos.map((todo, index) => (
+          <TodoItem key={index}>{todo}</TodoItem>
+        ))}
+      </TodoList>
     </PageWrapper>
   );
 };
